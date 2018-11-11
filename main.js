a964b7c92f2921f2105a17b3730512c34a5206fe
Angel <angelr1076@gmail.com>
2018-11-10 20:49:09 -0500

    added tweet button and links

--- main.js
+++ main.js
@@ -4,12 +4,11 @@ const authorBox = document.querySelector("#book");
 const quoteButton = document.querySelector("#quote-button");
 const twitterButton = document.querySelector("#tweet-button");
 
-
 const fetchThis =  () => {
     fetch('https://uncovered-treasure-v1.p.mashape.com/random', {
         headers: new Headers({
         'Accept':'application/json',
-        "X-Mashape-Key": "2UsjshuNOImshdbT4jg9bKJnoNI1p1LoMYujsnVlcNzqy7hEkY"
+        "X-Mashape-Key": "2UsjshuNOImshdbT4jg9bKJnoNI1p1LoMYujsnVlcNzqy7hEkY",
         })
     }
 )
@@ -27,9 +26,10 @@ const fetchThis =  () => {
     quoteButton.addEventListener("click", fetchThis);
     twitterButton.addEventListener("click", tweetQuote);
 
-    function tweetQuote(data) {
-        twitterButton.attr("href", `https://twitter.com/intent/tweet?text="${data.results[0].text}"  - ${data.results[0].context}`).attr('target', '_blank');
+    tweetQuote = data => {
+        data.getAttribute("href", `https://twitter.com/intent/tweet?text= ${data.results[0].text} ${data.results[0].context}`).getAttribute('target', '_blank');
     }
+    
 }
 
 fetchThis();
