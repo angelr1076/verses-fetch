19c2ec83256be46cccf05829211f1cc1a21073b5
Angel <angelr1076@gmail.com>
2018-11-12 13:43:22 -0500

    added random background colors and refactored quote and color functions

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const header = document.querySelector(".title");
 let quoteBox = document.querySelector("#quote");
 let authorBox = document.querySelector("#book");
+let thisBody = document.querySelector("body");
 const quoteButton = document.querySelector("#quote-button");
 const tweetButton = document.querySelector("#tweet-button");
 const endPoint = 'https://uncovered-treasure-v1.p.mashape.com/random';
@@ -21,18 +22,30 @@ const fetchThis = () => {
     .then (data => {
         quoteBox.innerHTML = `${data.results[0].text} - ${data.results[0].context}`;
         stringQuote = quoteBox.innerText;
+        fetchQuote();
         tweetQuote(stringQuote);
     })
     .catch = error => console.log(error)
 }
 
-quoteButton.addEventListener('click', fetchThis);
-
-let tweetQuote = quote => {
+const tweetQuote = quote => {
     tweetButton.setAttribute('href', `https://twitter.com/share?text=${quote}`);
 }
 
 fetchThis();
+
+let fetchQuote = () => {
+    quoteButton.addEventListener('click', fetchThis);
+    quoteButton.addEventListener('click', changeColors);
+}
+
+let changeColors = () => {
+    let colors = ["#330066", "#b0d996", "#ff66c1", "#1d4457", "#cc0000", "#f2de15", "#94cb71", "#8187ff", "#efd9a7"];
+
+    newColor = colors[Math.floor(Math.random() * colors.length)];
+    
+    thisBody.style.backgroundColor = newColor;
+}
 
   
   
