0055653b4121cd3e6d539efb5a9a038499fe7950
Angel <angelr1076@gmail.com>
2019-02-17 17:54:36 -0500

    switched calls from fetch to async/await

--- main.js
+++ main.js
@@ -1,49 +1,54 @@
-const header = document.querySelector(".title");
-let quoteBox = document.querySelector("#quote");
-let authorBox = document.querySelector("#book");
-let thisBody = document.querySelector("body");
-const quoteButton = document.querySelector("#quote-button");
-const tweetButton = document.querySelector("#tweet-button");
-const endPoint = 'https://uncovered-treasure-v1.p.mashape.com/random';
+const header = document.querySelector('.title')
+const quoteBox = document.querySelector('#quote')
+const authorBox = document.querySelector('#book')
+const thisBody = document.querySelector('body')
+const quoteButton = document.querySelector('#quote-button')
+const tweetButton = document.querySelector('#tweet-button')
+const endPoint = 'https://uncovered-treasure-v1.p.mashape.com/random'
 
-const fetchThis = () => {
-    fetch(endPoint, {
-        headers: new Headers({
-        'Accept':'application/json',
-        "X-Mashape-Key": "2UsjshuNOImshdbT4jg9bKJnoNI1p1LoMYujsnVlcNzqy7hEkY",
-        })
-    }
-)
-    .then(response => {
-        if(response.ok){
-            return response.json()
-        }
+const fetchThis = async () => {
+  try {
+    const result = await fetch(endPoint, {
+      headers: new Headers({
+        Accept: 'application/json',
+        'X-Mashape-Key': '2UsjshuNOImshdbT4jg9bKJnoNI1p1LoMYujsnVlcNzqy7hEkY'
+      })
     })
-    .then (data => {
-        quoteBox.innerHTML = `${data.results[0].text} - ${data.results[0].context}`;
-        stringQuote = quoteBox.innerText;
-        fetchQuote();
-        tweetQuote(stringQuote);
-    })
-    .catch = error => console.log(error)
+    const data = await result.json()
+    quoteBox.innerHTML = `${data.results[0].text} - ${data.results[0].context}`
+    stringQuote = quoteBox.innerText
+    fetchQuote()
+    tweetQuote(stringQuote)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 const tweetQuote = quote => {
-    tweetButton.setAttribute('href', `https://twitter.com/share?text=${quote}`);
+  tweetButton.setAttribute('href', `https://twitter.com/share?text=${quote}`)
 }
 
 let fetchQuote = () => {
-    quoteButton.addEventListener('click', fetchThis);
-    quoteButton.addEventListener('click', changeColors);
+  quoteButton.addEventListener('click', fetchThis)
+  quoteButton.addEventListener('click', changeColors)
 }
 
 let changeColors = () => {
-    const colors = ["#330066", "#b0d996", "#ff66c1", "#1d4457", "#cc0000", "#f2de15", "#94cb71", "#8187ff", "#efd9a7", "#bada55", "#819699"];
-    let newColor = colors[Math.floor(Math.random() * colors.length)];
-    thisBody.style.backgroundColor = newColor;
+  const colors = [
+    '#330066',
+    '#b0d996',
+    '#ff66c1',
+    '#1d4457',
+    '#cc0000',
+    '#f2de15',
+    '#94cb71',
+    '#8187ff',
+    '#efd9a7',
+    '#bada55',
+    '#819699'
+  ]
+  let newColor = colors[Math.floor(Math.random() * colors.length)]
+  thisBody.style.backgroundColor = newColor
 }
 
-fetchThis();
-
-  
-  
+fetchThis()
